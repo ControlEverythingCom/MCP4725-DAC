e6f90e182c76e8c73b229798bf26b1dc8fe231ef
telliottosceola <travis@controlanything.com>
2017-10-25 11:51:43 -0500

    initial upload

---
+++ MCP4725.h
@@ -0,0 +1,25 @@
+#include "spark_wiring_i2c.h"
+#include "spark_wiring_constants.h"
+#include "spark_wiring_usbserial.h"
+class MCP4725{
+public:
+
+bool begin();
+
+bool setOutputVoltage(float voltage, bool eeprom);
+
+bool setOutputRaw(int point, bool eeprom);
+
+void setInputVoltage(float voltage);
+
+private:
+
+int address = 0x60;
+
+int writeDacReg = 0x40;
+
+int writeDacEEPROMReg = 0x60;
+
+float inputVoltage = 0;
+
+};
